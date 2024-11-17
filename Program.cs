932ce26028c976ed55fd32464cc7f451337421d7
AXCWG <xiey0@qq.com>
2024-11-18 03:33:07 +0800

    fixes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using Fleck;
@@ -7,11 +8,10 @@ namespace WorldIO;
 
 class Income
 {
-    [JsonPropertyName("username")]
-    public String username;
-    [JsonPropertyName("msg")]
-    public String msg; 
+    [JsonPropertyName("username")] public String username;
+    [JsonPropertyName("msg")] public String msg;
 }
+
 class Program
 {
     static void Main(string[] args)
@@ -19,6 +19,7 @@ class Program
         List<IWebSocketConnection> sockets = new List<IWebSocketConnection>();
         var server = new WebSocketServer("wss://0.0.0.0:8181");
         server.Certificate = new X509Certificate2("andyxie.cn.pfx", "cpbxx43o");
+        server.EnabledSslProtocols = SslProtocols.Tls12;
         server.Start(socket =>
         {
             socket.OnOpen = () =>
@@ -29,18 +30,26 @@ class Program
                 }
 
                 var histstr = File.ReadAllLines("log.txt");
+                String append = string.Empty;
                 foreach (var line in histstr)
                 {
-                    socket.Send(line);
-                    Thread.Sleep(10);
-
+                    append += line + Environment.NewLine;
                 }
-                Console.WriteLine($"Open! (For {socket.ConnectionInfo.Id})"); 
+
+                socket.Send(append);
+                Console.WriteLine(
+                    $"[{DateTime.UtcNow}UTC] Open! (For {socket.ConnectionInfo.Id}, {socket.ConnectionInfo.ClientIpAddress}:{socket.ConnectionInfo.ClientPort})");
+                File.AppendAllText("log_ex.txt",
+                    $"[{DateTime.UtcNow}UTC] Open! (For {socket.ConnectionInfo.Id}, {socket.ConnectionInfo.ClientIpAddress}:{socket.ConnectionInfo.ClientPort})\n");
                 sockets.Add(socket);
             };
             socket.OnClose = () =>
             {
-                Console.WriteLine("Close!");
+                Console.WriteLine(
+                    $"[{DateTime.UtcNow}UTC] Close! (For {socket.ConnectionInfo.Id}, {socket.ConnectionInfo.ClientIpAddress}:{socket.ConnectionInfo.ClientPort})");
+                File.AppendAllText("log_ex.txt",
+                    $"[{DateTime.UtcNow}UTC] Close! (For {socket.ConnectionInfo.Id}, {socket.ConnectionInfo.ClientIpAddress}:{socket.ConnectionInfo.ClientPort})\n");
+
                 sockets.Remove(socket);
             };
             socket.OnMessage = message =>
@@ -52,22 +61,25 @@ class Program
                     Income income = JsonSerializer.Deserialize<Income>(message, new JsonSerializerOptions()
                     {
                         IncludeFields = true,
-
                     })!;
                     foreach (var socket in sockets)
                     {
-                        socket.Send($"[{DateTime.UtcNow}] {income.username} said: {income.msg}");
+                        socket.Send($"[{DateTime.UtcNow}UTC] {income.username} said: {income.msg}");
                     }
-                    File.AppendAllText("log.txt", $"[{DateTime.UtcNow}] {income.username} said: {income.msg}\n");
+
+                    File.AppendAllText("log.txt", $"[{DateTime.UtcNow}UTC] {income.username} said: {income.msg}\n");
+                    File.AppendAllText("log_ex.txt",
+                        $"[{DateTime.UtcNow}UTC] {income.username}({socket.ConnectionInfo.ClientIpAddress}:{socket.ConnectionInfo.ClientPort}) said: {income.msg}\n");
 
 
                     Console.WriteLine(income.username);
                 }
                 catch (Exception e)
                 {
+                    Console.WriteLine(e);
+                    File.AppendAllText("log_ex.txt", $"[{DateTime.UtcNow}UTC] {e}\n");
                     socket.Send("Fuck you. ");
                 }
-                
             };
         });
         while (true)
